27443d48e943781a1b60e86e44a79b6b3ec1816d
kamitsuTW <kamitsu9103@gmail.com>
2020-11-04 17:16:21 +0800

    finish story update

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,10 @@
 class StoriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_story, only: [:edit, :update, :destroy]
 
+  def index
+    @stories = current_user.stories.order(created_at: :desc)
+  end
   def new
     @story = current_user.stories.new
   end
@@ -15,7 +19,22 @@ class StoriesController < ApplicationController
     end
   end
 
+  def edit
+    
+  end
+
+  def update
+    if @story.update(story_params)
+      redirect_to stories_path, notice: '故事新增成功'
+    else
+      render :edit
+    end
+  end
+
   private
+  def find_story
+    @story = current_user.stories.find(params[:id])
+  end
   def story_params
     params.require(:story).permit(:title, :content)
   end
