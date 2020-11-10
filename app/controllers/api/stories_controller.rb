9c8d3671f6fa291a81a7ea50da60653c87602e42
kamitsuTW <kamitsu9103@gmail.com>
2020-11-10 14:28:37 +0800

    [feature] editor - part 2

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -2,7 +2,7 @@ class Api::StoriesController < Api::BaseController
   before_action :find_story
 
   def clap
-    story.increment!(:clap)
+    @story.increment!(:clap)
     render json: {status: @story.clap}
   end
 
