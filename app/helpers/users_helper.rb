b4db728abb360c68e92e189a7ceb2ad075fd4c9b
kamitsuTW <kamitsu9103@gmail.com>
2020-11-11 17:31:48 +0800

    add/fix userspage

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,14 @@ module UsersHelper
   def avatar(user, size: '250')
     image_tag user.avatar.variant(resize: "#{size}x#{size}"), class: 'user-avatar' if user.avatar.attached?
   end
+
+  def user_role(current_user)
+    if current_user.role == 'vip_user'
+      "VIP會員"
+    elsif current_user.role == 'platinum_user'
+      "白金會員"
+    else
+      "不想說"
+    end
+  end
 end
