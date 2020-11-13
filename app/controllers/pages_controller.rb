b4f058bcfde57a42cfa122b3ba170daf23363d3b
kamitsuTW <kamitsu9103@gmail.com>
2020-11-13 17:56:00 +0800

    add/modify css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,10 @@ class PagesController < ApplicationController
   before_action :find_story, only: [:show]
   def index
     # @stories = Story.published.with_attached_cover_image.order(created_at: :desc).includes(:user)
-    @new_story = Story.published_stories.limit(1).order(created_at: :desc)
-    # @stories_followed_list = current_user.follow_ids
-    @stories_new_list = Story.published_stories.limit(3).offset(1).order(created_at: :desc)
+    @new_story_first = Story.published_stories.limit(1).order(created_at: :desc)
+    @new_stories_sec_list = Story.published_stories.limit(2).offset(1).order(created_at: :desc)
     @stories_hot_list = Story.published_stories.order(clap: :desc)
+    # @stories_followed_list = current_user.follow_ids
   end
 
   def show
